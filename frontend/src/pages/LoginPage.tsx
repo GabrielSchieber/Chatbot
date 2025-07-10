8335ff08ec64ddbdcb74687ff60c4cc1f95bdd5c
DimensionalArray <204504974+DimensionalArray@users.noreply.github.com>
2025-07-10 16:37:15 -0300

    Add initial templates and styles for log in and sign up pages

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,3 +1,13 @@
+import "./AuthPages.css"
+
 export default function LoginPage() {
-    return <div><h1>Login</h1></div>
+    return (
+        <form id="auth-form" method="post">
+            <h1 id="title-h1">Log in</h1>
+            <input id="email-input" type="email" name="email" placeholder="Email" />
+            <input id="password-input" type="password" name="password" placeholder="Password" />
+            <button id="submit-button">Login</button>
+            <p>Don't have an account? <a id="recommendation-a" href="/signup">Sign up!</a></p>
+        </form>
+    )
 }
--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -1,3 +1,13 @@
+import "./AuthPages.css"
+
 export default function SignupPage() {
-    return <div>Sign up</div>
+    return (
+        <form id="auth-form" method="post">
+            <h1 id="title-h1">Sign up</h1>
+            <input id="email-input" type="email" placeholder="Email" />
+            <input id="password-input" type="password" placeholder="Password" />
+            <button id="submit-button">Sign up</button>
+            <p>Already have an account? <a id="recommendation-a" href="/login">Log in!</a></p>
+        </form>
+    )
 }
