5fa6c839eb493591cb0bfe392b2beb3f16ad659c
DimensionalArray <204504974+DimensionalArray@users.noreply.github.com>
2025-07-16 17:12:32 -0300

    Refactor styles of settings panel in chat page Add labels for each entry in settings

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -147,15 +147,32 @@ export default function ChatPage() {
             {isSettingsVisible &&
                 <div id="settings-div">
                     <p id="settings-p">Settings</p>
+
                     <button id="close-settings-button" onClick={_ => setIsSettingsVisible(false)}>X</button>
-                    <select id="theme-select" value={theme} onChange={event => setTheme(event.target.value as Theme)}>
-                        <option value="system">System</option>
-                        <option value="light">Light</option>
-                        <option value="dark">Dark</option>
-                    </select>
-                    <button id="delete-chats-button" onClick={deleteChats}>Delete chats</button>
-                    <button id="delete-account-button" onClick={deleteAccount}>Delete account</button>
-                    <button id="logout-button" onClick={async _ => { await logout(); location.reload() }}>Log out</button>
+
+                    <div id="theme-select-div">
+                        <label id="theme-select-label">Theme</label>
+                        <select id="theme-select" value={theme} onChange={event => setTheme(event.target.value as Theme)}>
+                            <option value="system">System</option>
+                            <option value="light">Light</option>
+                            <option value="dark">Dark</option>
+                        </select>
+                    </div>
+
+                    <div id="delete-chats-button-div">
+                        <label id="delete-chats-button-label">Delete all chats</label>
+                        <button id="delete-chats-button" onClick={deleteChats}>Delete all</button>
+                    </div>
+
+                    <div id="delete-account-button-div">
+                        <label id="delete-account-button-label">Delete account</label>
+                        <button id="delete-account-button" onClick={deleteAccount}>Delete</button>
+                    </div>
+
+                    <div id="logout-button-div">
+                        <label id="logout-button-label">Delete all chats</label>
+                        <button id="logout-button" onClick={async _ => { await logout(); location.reload() }}>Log out</button>
+                    </div>
                 </div>
             }
 
