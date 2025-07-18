382486db500de82c3788a13801ffb380c0ba15c7
DimensionalArray <204504974+DimensionalArray@users.noreply.github.com>
2025-07-18 12:59:59 -0300

    Add tests for signup, login and logout REST API views and improve user test

--- backend/chat/tests.py
+++ backend/chat/tests.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate
+from django.contrib.auth.hashers import check_password
 from django.test import TestCase
 
 from chat.models import Chat, Message, User
@@ -8,6 +9,7 @@ class UserTests(TestCase):
         user = create_user()
         self.assertEqual(user.email, "test@example.com")
         self.assertNotEqual(user.password, "testpassword")
+        self.assertTrue(check_password("testpassword", user.password))
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
 
@@ -38,6 +40,56 @@ class MessageTests(TestCase):
         self.assertEqual(bot_message.text, "Hi!")
         self.assertTrue(user_message.is_user_message)
         self.assertFalse(bot_message.is_user_message)
+
+class ViewTests(TestCase):
+    def test_signup(self):
+        response = self.client.post("/api/signup/", {"email": "test@example.com", "password": "testpassword"})
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(User.objects.all().count(), 1)
+        user = User.objects.first()
+        self.assertEqual(user.email, "test@example.com")
+        self.assertNotEqual(user.password, "testpassword")
+        self.assertTrue(check_password("testpassword", user.password))
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+
+    def test_signup_with_existing_email(self):
+        create_user()
+        response = self.client.post("/api/signup/", {"email": "test@example.com", "password": "testpassword"})
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(User.objects.all().count(), 1)
+
+    def test_login(self):
+        create_user()
+        response = self.client.post("/api/login/", {"email": "test@example.com", "password": "testpassword"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["success"], True)
+
+    def test_login_with_invalid_credentials(self):
+        create_user()
+        response = self.client.post("/api/login/", {"email": "someemail@example.com", "password": "somepassword"})
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json()["error"], "Invalid credentials")
+
+        response = self.client.post("/api/login/", {"email": "test@example.com", "password": "somepassword"})
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json()["error"], "Invalid credentials")
+
+        response = self.client.post("/api/login/", {"email": "someemail@example.com", "password": "testpassword"})
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json()["error"], "Invalid credentials")
+
+    def test_logout(self):
+        create_user()
+        response = self.client.post("/api/login/", {"email": "test@example.com", "password": "testpassword"})
+        response = self.client.post("/api/logout/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["success"], True)
+
+    def test_logout_without_being_authenticated(self):
+        response = self.client.post("/api/logout/")
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json()["detail"], "Authentication credentials were not provided.")
 
 def create_user(email: str = "test@example.com", password: str = "testpassword") -> User:
     return User.objects.create_user(email = email, password = password)
