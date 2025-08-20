3712969f10621e76634c0a59bea1fdde9d4679eb
DimensionalArray <204504974+DimensionalArray@users.noreply.github.com>
2025-08-20 12:38:41 -0300

    Simplify login and signup pages to one component and use it in when routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router"
+
 import "./App.css"
+import AuthPage from "./components/AuthPage"
 import ChatPage from "./pages/ChatPage"
-import LoginPage from "./pages/LoginPage"
-import SignupPage from "./pages/SignupPage"
 import { useAuth } from "./utils/auth"
 
 export default function App() {
@@ -14,8 +14,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={isLoggedIn ? <ChatPage /> : <Navigate to="/login" replace />} />
         <Route path="/chat/:chatUUID" element={isLoggedIn ? <ChatPage /> : <Navigate to="/login" replace />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<AuthPage type="Login" />} />
+        <Route path="/signup" element={<AuthPage type="Signup" />} />
       </Routes>
     </Router>
   )
