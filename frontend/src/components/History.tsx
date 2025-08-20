ec9915dba5a6b807613694d3031e1dd20acdb124
DimensionalArray <204504974+DimensionalArray@users.noreply.github.com>
2025-08-20 20:08:25 -0300

    Improve styles of history panel elements Change color and add more padding to anchor and button elements

--- frontend/src/components/History.tsx
+++ frontend/src/components/History.tsx
@@ -13,6 +13,7 @@ export default function History({ chats, setChats }: {
     const { chatUUID } = useParams()
     const [renameChatUUID, setRenameChatUUID] = useState<string | null>(null)
     const [renameTitle, setRenameTitle] = useState("")
+    const [selectedDropdownIndex, setSelectedDropdownIndex] = useState(-1)
 
     function startRename(chat: Chat) {
         setRenameChatUUID(chat.uuid)
@@ -52,12 +53,13 @@ export default function History({ chats, setChats }: {
 
     return (
         <div className="history flex flex-col gap-1 w-full h-full p-1 overflow-x-hidden overflow-y-auto">
-            {chats.map(chat => (
+            {chats.map((chat, index) => (
                 <div
                     key={chat.uuid}
                     className={`
-                        flex group px-2 py-1 justify-between rounded hover:bg-gray-700 light:hover:bg-gray-300
-                        ${(chatUUID === chat.uuid || renameChatUUID === chat.uuid) && "bg-gray-600 light:bg-gray-400"}
+                        flex group px-2 py-1 justify-between rounded
+                        ${(chatUUID === chat.uuid || renameChatUUID === chat.uuid) && "bg-gray-700 light:bg-gray-400/40"}
+                        ${selectedDropdownIndex === index ? "bg-gray-600 light:bg-gray-300" : "hover:bg-gray-600 light:hover:bg-gray-300"}
                     `}
                 >
                     {renameChatUUID === chat.uuid ? (
@@ -79,23 +81,23 @@ export default function History({ chats, setChats }: {
                         </a>
                     )}
 
-                    <DropdownMenu.Root>
+                    <DropdownMenu.Root onOpenChange={o => setSelectedDropdownIndex(o ? index : -1)}>
                         <DropdownMenu.Trigger asChild>
                             <button className="hover:bg-gray-600 light:hover:bg-gray-400 outline-none rounded">
                                 <DotsVerticalIcon />
                             </button>
                         </DropdownMenu.Trigger>
 
-                        <DropdownMenu.Content className="bg-gray-700 light:bg-gray-300 p-2 rounded shadow-xl/30">
+                        <DropdownMenu.Content className="bg-gray-700 light:bg-gray-200 p-2 rounded-xl shadow-xl/30 translate-x-7" sideOffset={2}>
                             <DropdownMenu.Item
-                                className="p-2 text-center outline-none rounded hover:bg-gray-500/50"
+                                className="px-4 py-2 text-center outline-none rounded-xl select-none cursor-pointer hover:bg-gray-600 light:hover:bg-gray-300"
                                 onSelect={_ => startRename(chat)}
                             >
                                 Rename
                             </DropdownMenu.Item>
                             <ConfirmDialog
                                 trigger={
-                                    <button className="p-2 text-center outline-none rounded w-full hover:bg-red-400/40 text-red-400">
+                                    <button className="px-4 py-2 text-center outline-none rounded-xl w-full hover:bg-red-300/30 text-red-400 cursor-pointer">
                                         Delete
                                     </button>
                                 }
