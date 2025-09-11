ba964754557ea7641457cc00ca3e6e7917b2b83d
DimensionalArray <204504974+DimensionalArray@users.noreply.github.com>
2025-09-11 01:44:37 +0000

    Move sidebar fixtures definitions to utilities file and refactor sidebar tests

--- frontend/tests/sidebar.spec.ts
+++ frontend/tests/sidebar.spec.ts
@@ -1,83 +1,13 @@
-import { expect, test as base } from "@playwright/test"
-import { execSync } from "child_process"
+import { expect, Page } from "@playwright/test"
+import { test, User } from "./utils"
 
-type Message = {
-    text: string
-    is_from_user: boolean
-}
-
-type Chat = {
-    title: string
-    uuid: string
-    messages: Message[]
-}
-
-type User = {
-    email: string
-    password: string
-    chats: Chat[]
-}
-
-type TestFixtures = { user: User }
-
-const test = base.extend<TestFixtures>({
-    user: [
-        async ({ }, use) => {
-            const email = "test@example.com"
-            const password = "testpassword"
-
-            const chats: Chat[] = [
-                {
-                    title: "Greetings!",
-                    uuid: "",
-                    messages: [
-                        {
-                            text: "Hi!",
-                            is_from_user: true
-                        },
-                        {
-                            text: "Hello! How are you going?",
-                            is_from_user: false
-                        }
-                    ]
-                }
-            ]
-
-            function executeBackendCommand(args: string[]): string {
-                return execSync(`cd /app/backend && python manage.py ${args.join(" ")}`).toString().trim()
-            }
-
-            executeBackendCommand(["create_user", "--email", email, "--password", password])
-            for (const chat of chats) {
-                chat.uuid = executeBackendCommand(["create_chat", "--user-email", email, "--chat-title", chat.title])
-                for (const message of chat.messages) {
-                    executeBackendCommand([
-                        "create_message", "--user-email", email, "--chat-uuid", chat.uuid,
-                        "--message-text", `"${message.text}"`, "--message-is-from-user", message.is_from_user ? "True" : "False"
-                    ])
-                }
-            }
-
-            await use({ email, password, chats })
-        },
-        { scope: "worker", auto: true }
-    ]
-})
-
-test.beforeEach(async ({ page, user }) => {
-    await page.goto("/")
-    await page.waitForURL("/login")
-    await page.getByPlaceholder("Email").fill(user.email)
-    await page.getByPlaceholder("Password").fill(user.password)
-    await page.getByRole("button").click()
-    await page.waitForURL("/")
-})
-
-test("user can toggle sidebar", async ({ page }) => {
+test("user can toggle sidebar", async ({ page, users }) => {
     const toggleSidebarText = "Toggle Sidebar"
     const newChatText = "New Chat"
     const searchText = "Search"
     const settingsText = "Settings"
+
+    await login(page, users[0])
 
     const toggleSidebar = page.getByTestId("toggle-sidebar")
     const newChat = page.getByTestId("new-chat")
@@ -102,18 +32,32 @@ test("user can toggle sidebar", async ({ page }) => {
     await expect(settings).toContainText(settingsText)
 })
 
-test("user can open search", async ({ page, user }) => {
+test("user can open search", async ({ page, users }) => {
+    const user = users[1]
+    const chat = user.chats[0]
+    const messages = chat.messages
+
+    await login(page, user)
+
     await page.getByTestId("search").click()
 
-    const searchInput = page.getByPlaceholder("Search chats...")
-    await expect(searchInput).toBeVisible()
+    await expect(page.getByPlaceholder("Search chats...")).toBeVisible()
 
-    const searchEntry = page.getByRole("link", { "name": "Greetings!" })
+    const searchEntry = page.getByRole("link", { "name": chat.title })
     await expect(searchEntry).toBeVisible()
 
-    const closeButton = page.getByRole("button")
-    await expect(closeButton).toBeVisible()
+    const matches = searchEntry.getByRole("list")
+    await expect(matches.getByRole("listitem").first()).toHaveText(messages[0].text + "...")
+    await expect(matches.getByRole("listitem").last()).toHaveText(messages[1].text + "...")
 
-    await expect(page.getByText(user.chats[0].messages[0].text + "...")).toBeVisible()
-    await expect(page.getByText(user.chats[0].messages[1].text + "...")).toBeVisible()
+    await expect(page.getByRole("button")).toBeVisible()
 })
+
+async function login(page: Page, user: User) {
+    await page.goto("/")
+    await page.waitForURL("/login")
+    await page.getByPlaceholder("Email").fill(user.email)
+    await page.getByPlaceholder("Password").fill(user.password)
+    await page.getByRole("button").click()
+    await page.waitForURL("/")
+}
--- frontend/tests/utils.ts
+++ frontend/tests/utils.ts
@@ -1,4 +1,5 @@
-import { Page } from "@playwright/test"
+import { Page, test as base } from "@playwright/test"
+import { execSync } from "child_process"
 
 export function apiFetch(url: string, init: RequestInit) {
     return fetch(`http://localhost:8000${url}`, init)
@@ -19,4 +20,73 @@ export async function signupAndLogin(page: Page) {
 
     await page.click("button")
     await page.waitForURL("/")
+}
+
+export type Message = {
+    text: string
+    is_from_user: boolean
+}
+
+export type Chat = {
+    title: string
+    uuid: string
+    messages: Message[]
+}
+
+export type User = {
+    email: string
+    password: string
+    chats: Chat[]
+}
+
+export type TestFixtures = { users: User[] }
+
+export const test = base.extend<TestFixtures>({
+    users: [
+        async ({ }, use) => {
+            const user1 = createUser("test1@example.com", "testpassword", [])
+            const user2 = createUser(
+                "test2@example.com",
+                "testpassword",
+                [
+                    {
+                        title: "Greetings",
+                        uuid: "",
+                        messages: [
+                            {
+                                "text": "Hi!",
+                                is_from_user: true
+                            },
+                            {
+                                "text": "Hello! How are you doing?",
+                                is_from_user: true
+                            }
+                        ]
+                    }
+                ]
+            )
+            await use([user1, user2])
+        },
+        { scope: "worker", auto: true }
+    ]
+})
+
+function createUser(email: string, password: string, chats: Chat[]): User {
+    function executeBackendCommand(args: string[]): string {
+        return execSync(`cd /app/backend && python manage.py ${args.join(" ")}`).toString().trim()
+    }
+
+    executeBackendCommand(["create_user", "--email", email, "--password", password])
+
+    for (const chat of chats) {
+        chat.uuid = executeBackendCommand(["create_chat", "--user-email", email, "--chat-title", `"${chat.title}"`])
+        for (const message of chat.messages) {
+            executeBackendCommand([
+                "create_message", "--user-email", email, "--chat-uuid", chat.uuid,
+                "--message-text", `"${message.text}"`, "--message-is-from-user", message.is_from_user ? "True" : "False"
+            ])
+        }
+    }
+
+    return { email, password, chats }
 }
