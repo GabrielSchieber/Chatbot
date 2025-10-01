7e35173ce99b3ea4b5b0b9469e19c71a1bc47a92
DimensionalArray <204504974+DimensionalArray@users.noreply.github.com>
2025-10-01 01:59:23 +0000

    Add prompt bar buttons

--- frontend/src/components/Prompt.tsx
+++ frontend/src/components/Prompt.tsx
@@ -1,8 +1,10 @@
-import { useState } from "react"
+import { ArrowUpIcon, BoxModelIcon, CheckIcon, PlusIcon } from "@radix-ui/react-icons"
+import { DropdownMenu } from "radix-ui"
+import { useState, type ReactNode } from "react"
 import { useNavigate, useParams } from "react-router"
 
 import { newMessage } from "../utils/api"
-import type { Message, MessageFile } from "../types"
+import type { Message, MessageFile, Model } from "../types"
 
 export default function Prompt({ setMessages }: { setMessages: React.Dispatch<React.SetStateAction<Message[]>> }) {
     const { chatUUID } = useParams()
@@ -10,9 +12,10 @@ export default function Prompt({ setMessages }: { setMessages: React.Dispatch<Re
 
     const [text, setText] = useState("")
     const [files, setFiles] = useState<File[]>([])
+    const [model, setModel] = useState<Model>("SmolLM2-135M")
 
     function sendMessage() {
-        newMessage(chatUUID || "", text, "SmolLM2-135M", files).then(response => {
+        newMessage(chatUUID || "", text, model, files).then(response => {
             if (response.ok) {
                 setMessages(previous => {
                     previous = [...previous]
@@ -27,7 +30,7 @@ export default function Prompt({ setMessages }: { setMessages: React.Dispatch<Re
                     }))
 
                     previous.push({ id: highestID + 1, text, files: newFiles, is_from_user: true, model: undefined })
-                    previous.push({ id: highestID + 2, text: "", files: [], is_from_user: false, model: "SmolLM2-135M" })
+                    previous.push({ id: highestID + 2, text: "", files: [], is_from_user: false, model })
 
                     return previous
                 })
@@ -51,10 +54,54 @@ export default function Prompt({ setMessages }: { setMessages: React.Dispatch<Re
     }
 
     return (
-        <div className="flex flex-col w-[60vw] mt-auto mb-5 rounded-3xl bg-gray-700 light:bg-gray-300">
-            <Attachments files={files.map((f, i) => ({ id: i, name: f.name, content_size: f.size, content_type: f.type }))} />
-            <TextArea text={text} setText={setText} sendMessage={sendMessageWithEvent} />
+        <div className="flex w-[60vw] mt-auto mb-5 px-5 rounded-3xl bg-gray-700 light:bg-gray-300">
+            <div className="flex gap-2">
+                <Button icon={<PlusIcon className="size-6" />} />
+                <Dropdown icon={<BoxModelIcon className="size-6" />} model={model} setModel={setModel} />
+            </div>
+            <div className="flex flex-1 flex-col">
+                <Attachments files={files.map((f, i) => ({ id: i, name: f.name, content_size: f.size, content_type: f.type }))} />
+                <TextArea text={text} setText={setText} sendMessage={sendMessageWithEvent} />
+            </div>
+            <Button icon={<ArrowUpIcon className="size-6" />} />
         </div>
+    )
+}
+
+function Button({ icon }: { icon: ReactNode }) {
+    return <button>{icon}</button>
+}
+
+function Dropdown({ icon, model, setModel }: {
+    icon: ReactNode
+    model: Model
+    setModel: React.Dispatch<React.SetStateAction<Model>>
+}) {
+    function Item({ m }: { m: Model }) {
+        return (
+            <DropdownMenu.Item
+                className="p-2 rounded-md cursor-pointer hover:bg-gray-700 light:hover:bg-gray-300"
+                onClick={_ => setModel(m)}
+            >
+                <div className="flex gap-2 items-center">
+                    {m}{m === model && <CheckIcon className="size-5" />}
+                </div>
+            </DropdownMenu.Item>
+        )
+    }
+
+    const models: Model[] = ["SmolLM2-135M", "SmolLM2-360M", "SmolLM2-1.7B", "Moondream"]
+
+    return (
+        <DropdownMenu.Root>
+            <DropdownMenu.Trigger>{icon}</DropdownMenu.Trigger>
+
+            <DropdownMenu.Content className="flex flex-col gap-1 p-1 rounded-md bg-gray-800 light:bg-gray-200">
+                {models.map(m => (
+                    <Item key={m} m={m} />
+                ))}
+            </DropdownMenu.Content>
+        </DropdownMenu.Root>
     )
 }
 
@@ -83,7 +130,7 @@ function TextArea({ text, setText, sendMessage }: {
 }) {
     return (
         <textarea
-            className="px-6 py-1 content-center rounded-3xl resize-none outline-none bg-gray-700 light:bg-gray-300"
+            className="px-3 py-1 content-center rounded-3xl resize-none outline-none bg-gray-700 light:bg-gray-300"
             placeholder="Type your message here..."
             value={text}
             onChange={e => setText(e.target.value)}
