14f1e0b330152236ef17cd8fcf83d87236a3b127
DimensionalArray <204504974+DimensionalArray@users.noreply.github.com>
2025-10-03 16:08:00 +0000

    Display number of files and file size in attachments

--- frontend/src/components/ui/Attachments.tsx
+++ frontend/src/components/ui/Attachments.tsx
@@ -1,4 +1,6 @@
 import { Cross1Icon, FileIcon } from "@radix-ui/react-icons"
+
+import { MAX_FILE_SIZE, MAX_FILES } from "../Chat"
 import { getFileSize, getFileType } from "../../utils/file"
 import type { MessageFile } from "../../types"
 
@@ -8,6 +10,16 @@ export default function Attachments({ files, onRemove, onRemoveAll }: { files: M
             {files.map(f => (
                 onRemove ? <Attachment key={f.id} file={f} onRemove={() => onRemove(f)} /> : <Attachment key={f.id} file={f} />
             ))}
+            {files.length > 0 &&
+                <div className="flex gap-2">
+                    <p className="px-2.5 rounded-lg bg-gray-800 light:bg-gray-200" >
+                        Files: {files.length}/{MAX_FILES}
+                    </p>
+                    <p className="px-2.5 rounded-lg bg-gray-800 light:bg-gray-200">
+                        Size: {getFileSize(files.map(f => f.content_size).reduce((a, c) => a + c, 0))}/{getFileSize(MAX_FILE_SIZE)}
+                    </p>
+                </div>
+            }
             {onRemoveAll && files.length > 0 &&
                 <button className="absolute right-0 p-1 rounded-3xl cursor-pointer hover:bg-red-500/40" onClick={onRemoveAll}>
                     <Cross1Icon className="size-3.5" />
