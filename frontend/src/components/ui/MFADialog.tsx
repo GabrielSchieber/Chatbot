f6366fd8791c9ee32e18443f84528eb18f3c4382
DimensionalArray <204504974+DimensionalArray@users.noreply.github.com>
2025-10-13 20:20:11 +0000

    Add a download button that allows the user to download MFA recovery codes as a .txt file

--- frontend/src/components/ui/MFADialog.tsx
+++ frontend/src/components/ui/MFADialog.tsx
@@ -1,4 +1,4 @@
-import { CheckIcon, CopyIcon, Cross1Icon } from "@radix-ui/react-icons"
+import { CheckIcon, CopyIcon, Cross1Icon, DownloadIcon } from "@radix-ui/react-icons"
 import { QRCodeCanvas } from "qrcode.react"
 import { Dialog } from "radix-ui"
 import { useEffect, useState, type Dispatch, type SetStateAction } from "react"
@@ -195,21 +195,40 @@ function EnabledDialog({ backupCodes }: { backupCodes: string[] }) {
     const [isCopyButtonChecked, setIsCopyButtonChecked] = useState(false)
     const [hasConfirmedBackup, setHasConfirmedBackup] = useState(false)
 
+    function handleDownload() {
+        const blob = new Blob([backupCodes.join("\n")], { type: "text/plain" })
+        const url = URL.createObjectURL(blob)
+
+        const a = document.createElement("a")
+        a.href = url
+        a.download = "Recovery Codes.txt"
+        a.click()
+
+        URL.revokeObjectURL(url)
+    }
+
     return (
         <div className="flex flex-col gap-2 p-2 items-center rounded-xl bg-gray-700/30 light:bg-gray-300/30">
-            <h3>Backup codes</h3>
-            <p>Store these somewhere safe — each can be used once:</p>
+            <p>Make sure to backup the following 10 recovery codes (each one can only be used once):</p>
             <ul className="relative flex flex-col w-sm px-4 py-2 rounded-xl bg-gray-700 light:bg-gray-300">
-                <button
-                    className="absolute right-0 mr-2 p-1 rounded cursor-pointer hover:bg-gray-600 light:hover:bg-gray-400"
-                    onClick={_ => {
-                        navigator.clipboard.writeText(backupCodes.join("\n"))
-                        setIsCopyButtonChecked(true)
-                        setTimeout(() => setIsCopyButtonChecked(false), 2000)
-                    }}
-                >
-                    {isCopyButtonChecked ? <CheckIcon className="size-4.5" /> : <CopyIcon className="size-4.5" />}
-                </button>
+                <div className="absolute right-0 flex flex-col mr-2 gap-1">
+                    <button
+                        className="p-1.5 rounded items-center justify-center cursor-pointer hover:bg-gray-600 light:hover:bg-gray-400"
+                        onClick={_ => {
+                            navigator.clipboard.writeText(backupCodes.join("\n"))
+                            setIsCopyButtonChecked(true)
+                            setTimeout(() => setIsCopyButtonChecked(false), 2000)
+                        }}
+                    >
+                        {isCopyButtonChecked ? <CheckIcon className="size-4.5" /> : <CopyIcon className="size-4.5" />}
+                    </button>
+                    <button
+                        className="p-1.5 rounded items-center justify-center cursor-pointer hover:bg-gray-600 light:hover:bg-gray-400"
+                        onClick={handleDownload}
+                    >
+                        <DownloadIcon className="size-4.5" />
+                    </button>
+                </div>
                 {backupCodes.map(c =>
                     <li key={c} className="font-mono">{c}</li>
                 )}
