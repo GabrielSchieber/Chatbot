aba62110e078e78a5a78e66d692da4f7741ceed7
DimensionalArray <204504974+DimensionalArray@users.noreply.github.com>
2025-11-10 12:30:01 -0300

    Setup OriginValidator for web socket connections

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,6 +1,8 @@
 import os
 
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import OriginValidator
+from django.conf import settings
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
@@ -9,4 +11,4 @@ django_asgi_app = get_asgi_application()
 
 from chat.routing import websocket_urlpatterns
 
-application = ProtocolTypeRouter({"http": django_asgi_app, "websocket": URLRouter(websocket_urlpatterns)})
+application = ProtocolTypeRouter({"http": django_asgi_app, "websocket": OriginValidator(URLRouter(websocket_urlpatterns), settings.ALLOWED_HOSTS)})
