30b1a8fbf6f06e1dac0154b5e8bcb031a85927d4
DimensionalArray <204504974+DimensionalArray@users.noreply.github.com>
2025-11-22 22:30:19 +0000

    Simplify how messages are structured

--- frontend/src/components/Messages.tsx
+++ frontend/src/components/Messages.tsx
@@ -9,6 +9,7 @@ import Attachments from "./Attachments"
 import { CopyButton, EditButton, RegenerateButton } from "./Buttons"
 import Editor from "./Editor"
 import { useChat } from "../providers/ChatProvider"
+import type { MessageFile, Model } from "../utils/types"
 
 export default function Messages() {
     const { chatUUID } = useParams()
@@ -85,114 +86,113 @@ export default function Messages() {
     }, [shouldScrollToBottom, messages.at(-1)?.text])
 
     return (
-        <div
-            ref={ref}
-            className={`flex flex-col w-full px-3 pt-10 pb-5 items-center overflow-y-auto duration-500 ${!chatUUID ? "h-[35%]" : "h-[100%]"} ${!isMobile && "items-center"}`}
-            onScroll={handleScroll}
-        >
-            {messages.map((m, i) => (
-                <div
-                    key={m.id}
-                    className={`
-                        flex flex-col gap-0.5 ${isMobile ? "w-full" : "w-[60vw]"}
-                        ${editingMessageIndex === i && isMobile ? "items-center" : m.is_from_user ? "items-end" : "items-start"}
-                    `}
-                >
-                    {editingMessageIndex === i ? (
-                        <Editor index={editingMessageIndex} setIndex={setEditingMessageIndex} />
+        <div ref={ref} className="flex flex-col size-full px-2 py-10 items-center overflow-y-auto" onScroll={handleScroll}>
+            <div className={`flex flex-col gap-3 ${isMobile ? "w-full" : "w-[60vw]"}`}>
+                {messages.map((m, i) =>
+                    editingMessageIndex === i ? (
+                        <Editor index={i} setIndex={setEditingMessageIndex} />
+                    ) : m.is_from_user ? (
+                        <UserMessage index={i} text={m.text} files={m.files} onEditClick={() => setEditingMessageIndex(i)} />
                     ) : (
-                        m.is_from_user ? (
-                            <>
-                                <div
-                                    className={`flex flex-col gap-1 min-w-20 px-3 py-2 rounded-xl bg-gray-800 light:bg-gray-200 ${isMobile ? "max-w-full" : "max-w-[80%]"}`}
-                                    data-testid={`message-${i}`}
-                                >
-                                    {m.files.length > 0 &&
-                                        <div className="flex flex-wrap gap-2 p-2 rounded-xl border bg-gray-700 light:bg-gray-300 border-gray-200 light:border-gray-800">
-                                            <Attachments files={m.files} />
-                                        </div>
-                                    }
-                                    <div className="w-full wrap-anywhere whitespace-pre-wrap">
-                                        {m.text}
-                                    </div>
-                                </div>
+                        <BotMessage index={i} text={m.text} model={m.model} />
+                    )
+                )}
+            </div>
+        </div>
+    )
+}
 
-                                <div className="flex gap-1">
-                                    <EditButton onClick={() => setEditingMessageIndex(i)} />
-                                    <CopyButton text={m.text} data-testid="copy" />
-                                </div>
-                            </>
-                        ) : (
-                            <>
-                                <div className="w-full wrap-anywhere whitespace-pre-wrap" data-testid={`message-${i}`}>
-                                    <ReactMarkdown
-                                        children={m.text}
-                                        remarkPlugins={[remarkGfm]}
-                                        rehypePlugins={[rehypeHighlight]}
-                                        components={{
-                                            code({ node, className, children, ...props }) {
-                                                const isInline = !className
-                                                if (isInline) {
-                                                    return (
-                                                        <code className="px-1 bg-gray-700 light:bg-gray-300 rounded" {...props}>
-                                                            {children}
-                                                        </code>
-                                                    )
-                                                }
-                                                return <code className={className} {...props}>{children}</code>
-                                            },
-
-                                            pre({ node, children, ...props }) {
-                                                const [copied, setCopied] = useState(false)
-
-                                                function copyCodeBlock(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-                                                    const codeBlock = e.currentTarget?.parentElement?.parentElement?.querySelector("pre")
-                                                    navigator.clipboard.writeText(codeBlock?.textContent || "")
-                                                    setCopied(true)
-                                                    setTimeout(() => setCopied(false), 2000)
-                                                }
-
-                                                const childArray = React.Children.toArray(children)
-                                                const codeNode = childArray[0] as ReactElement<{ className?: string, children?: React.ReactNode }>
-
-                                                const className = codeNode?.props.className || ""
-                                                const languageMatch = /language-(\w+)/.exec(className)
-                                                const language = languageMatch ? languageMatch[1] : "code"
-
-                                                return (
-                                                    <div className="rounded-lg overflow-hidden my-2">
-                                                        <div className="flex items-center justify-between bg-gray-700 light:bg-gray-300 px-4 py-1">
-                                                            <p className="text-sm m-0">{language}</p>
-                                                            <button
-                                                                className="
-                                                                    flex items-center gap-1 px-2 py-[2px] text-xs cursor-pointer
-                                                                    rounded hover:bg-gray-800 light:hover:bg-gray-200
-                                                                "
-                                                                onClick={copyCodeBlock}
-                                                            >
-                                                                {copied ? <CheckIcon className="size-4.5" /> : <CopyIcon className="size-4.5" />}
-                                                                {copied ? "Copied" : "Copy"}
-                                                            </button>
-                                                        </div>
-                                                        <pre className="overflow-x-auto m-0" {...props}>
-                                                            {children}
-                                                        </pre>
-                                                    </div>
-                                                )
-                                            }
-                                        }}
-                                    />
-                                </div>
-
-                                <div className="flex gap-1">
-                                    <CopyButton text={m.text} />
-                                    <RegenerateButton index={i} model={m.model} />
-                                </div>
-                            </>
-                        )
-                    )}
+function UserMessage({ index, text, files, onEditClick }: { index: number, text: string, files: MessageFile[], onEditClick: VoidFunction }) {
+    return (
+        <div className="flex flex-col self-end rounded-2xl bg-gray-800 light:bg-gray-200">
+            <div className="px-4 py-3 wrap-anywhere whitespace-pre-wrap" data-testid={`message-${index}`}>
+                {files.length > 0 &&
+                    <div className="flex flex-wrap gap-2 p-2 rounded-xl border bg-gray-700 light:bg-gray-300 border-gray-200 light:border-gray-800">
+                        <Attachments files={files} />
+                    </div>
+                }
+                <div className="wrap-anywhere whitespace-pre-wrap">
+                    {text}
                 </div>
-            ))}
+            </div>
+
+            <div className="border border-gray-500" />
+
+            <div className="flex self-end gap-1 px-2 py-1">
+                <EditButton onClick={onEditClick} />
+                <CopyButton text={text} data-testid="copy" />
+            </div>
+        </div>
+    )
+}
+
+function BotMessage({ index, text, model }: { index: number, text: string, model: Model | null }) {
+    return (
+        <div className="flex flex-col rounded-xl">
+            <div className="wrap-anywhere whitespace-pre-wrap" data-testid={`message-${index}`}>
+                <ReactMarkdown
+                    children={text}
+                    remarkPlugins={[remarkGfm]}
+                    rehypePlugins={[rehypeHighlight]}
+                    components={{
+                        code({ node, className, children, ...props }) {
+                            const isInline = !className
+                            if (isInline) {
+                                return (
+                                    <code className="px-1 bg-gray-700 light:bg-gray-300 rounded" {...props}>
+                                        {children}
+                                    </code>
+                                )
+                            }
+                            return <code className={className} {...props}>{children}</code>
+                        },
+
+                        pre({ node, children, ...props }) {
+                            const [copied, setCopied] = useState(false)
+
+                            function copyCodeBlock(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+                                const codeBlock = e.currentTarget?.parentElement?.parentElement?.querySelector("pre")
+                                navigator.clipboard.writeText(codeBlock?.textContent || "")
+                                setCopied(true)
+                                setTimeout(() => setCopied(false), 2000)
+                            }
+
+                            const childArray = React.Children.toArray(children)
+                            const codeNode = childArray[0] as ReactElement<{ className?: string, children?: React.ReactNode }>
+
+                            const className = codeNode?.props.className || ""
+                            const languageMatch = /language-(\w+)/.exec(className)
+                            const language = languageMatch ? languageMatch[1] : "code"
+
+                            return (
+                                <div className="rounded-lg overflow-hidden my-2">
+                                    <div className="flex items-center justify-between bg-gray-700 light:bg-gray-300 px-4 py-1">
+                                        <p className="text-sm m-0">{language}</p>
+                                        <button
+                                            className="
+                                                flex items-center gap-1 px-2 py-[2px] text-xs cursor-pointer
+                                                rounded hover:bg-gray-800 light:hover:bg-gray-200
+                                            "
+                                            onClick={copyCodeBlock}
+                                        >
+                                            {copied ? <CheckIcon className="size-4.5" /> : <CopyIcon className="size-4.5" />}
+                                            {copied ? "Copied" : "Copy"}
+                                        </button>
+                                    </div>
+                                    <pre className="overflow-x-auto m-0" {...props}>
+                                        {children}
+                                    </pre>
+                                </div>
+                            )
+                        }
+                    }}
+                />
+            </div>
+
+            <div className="flex gap-1">
+                <CopyButton text={text} />
+                <RegenerateButton index={index} model={model} />
+            </div>
         </div>
     )
 }
