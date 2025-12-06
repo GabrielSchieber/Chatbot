1e6f52ad904def7e6d8119f1c39a14114f2cbc66
DimensionalArray <204504974+DimensionalArray@users.noreply.github.com>
2025-12-06 19:41:54 +0000

    Fix type definitions in function parameters in totp_utils.py

--- backend/chat/totp_utils.py
+++ backend/chat/totp_utils.py
@@ -14,10 +14,10 @@ def encrypt_secret(secret: str):
 def decrypt_secret(encrypted_secret: bytes) -> str:
     return get_cipher().decrypt(encrypted_secret).decode()
 
-def verify_secret(encrypted_secret: str, code: str):
+def verify_secret(encrypted_secret: bytes, code: str):
     return pyotp.TOTP(decrypt_secret(encrypted_secret)).verify(code, valid_window = 1)
 
-def generate_code(encrypted_secret: str):
+def generate_code(encrypted_secret: bytes):
     return pyotp.TOTP(decrypt_secret(encrypted_secret)).now()
 
 def generate_secret():
