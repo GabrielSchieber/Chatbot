a5d56e4adc199ec5cba9a7684aebdb0076706f2d
DimensionalArray <204504974+DimensionalArray@users.noreply.github.com>
2025-12-11 15:57:54 +0000

    Fix ChatConsumer tests from failing when all tests are run in parallel Make these tests use database_sync_to_async instead of sync_to_async and use transactional database pytest fixture

--- backend/chat/tests/consumers.py
+++ backend/chat/tests/consumers.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from typing import Any, Iterable
 
 import pytest
-from asgiref.sync import sync_to_async
+from channels.db import database_sync_to_async
 from channels.layers import get_channel_layer
 from channels.testing import WebsocketCommunicator
 from freezegun import freeze_time
@@ -16,16 +16,16 @@ from ..consumers import ChatConsumer
 from ..models import User
 from ..tasks import opened_chats
 
-@pytest.mark.parametrize("anyio_backend", ["asyncio"])
+@pytest.mark.asyncio
 async def test_reject_unauthenticated_connection():
     ws = get_communicator()
     connected, subprotocol = await ws.connect()
     assert connected is False
     assert subprotocol == 401
 
-@pytest.mark.parametrize("anyio_backend", ["asyncio"])
-async def test_reject_connection_with_tampered_cookie(db):
-    user = await sync_to_async(create_user)()
+@pytest.mark.asyncio
+async def test_reject_connection_with_tampered_cookie(transactional_db):
+    user = await database_sync_to_async(create_user)()
     cookie = get_access_cookie_for_user(user)
     cookie = cookie[:-1] + ("A" if cookie[-1] != "A" else "B")
 
@@ -34,11 +34,11 @@ async def test_reject_connection_with_tampered_cookie(db):
     assert connected is False
     assert subprotocol == 401
 
-@pytest.mark.parametrize("anyio_backend", ["asyncio"])
-async def test_reject_connection_with_expired_cookie(db):
+@pytest.mark.asyncio
+async def test_reject_connection_with_expired_cookie(transactional_db):
     time_to_freeze = datetime(2025, 1, 1, 12)
     with freeze_time(time_to_freeze):
-        user = await sync_to_async(create_user)()
+        user = await database_sync_to_async(create_user)()
         cookie = AccessToken.for_user(user)
 
     with freeze_time(time_to_freeze + cookie.lifetime):
@@ -47,13 +47,13 @@ async def test_reject_connection_with_expired_cookie(db):
         assert connected is False
         assert subprotocol == 401
 
-@pytest.mark.parametrize("anyio_backend", ["asyncio"])
-async def test_accept_authenticated_connection(db):
+@pytest.mark.asyncio
+async def test_accept_authenticated_connection(transactional_db):
     _, ws = await connect_to_communicator_with_user()
     await ws.disconnect()
 
-@pytest.mark.parametrize("anyio_backend", ["asyncio"])
-async def test_join_chat_group(db):
+@pytest.mark.asyncio
+async def test_join_chat_group(transactional_db):
     user, ws = await connect_to_communicator_with_user()
     chat = await user.chats.acreate(title = "Chat")
 
@@ -62,8 +62,8 @@ async def test_join_chat_group(db):
 
     await ws.disconnect()
 
-@pytest.mark.parametrize("anyio_backend", ["asyncio"])
-async def test_receive_send_token_event(db):
+@pytest.mark.asyncio
+async def test_receive_send_token_event(transactional_db):
     user, ws = await connect_to_communicator_with_user()
     chat = await user.chats.acreate(title = "Chat")
 
@@ -78,8 +78,8 @@ async def test_receive_send_token_event(db):
 
     await ws.disconnect()
 
-@pytest.mark.parametrize("anyio_backend", ["asyncio"])
-async def test_receive_send_message_event(db):
+@pytest.mark.asyncio
+async def test_receive_send_message_event(transactional_db):
     user, ws = await connect_to_communicator_with_user()
     chat = await user.chats.acreate(title = "Chat")
 
@@ -94,8 +94,8 @@ async def test_receive_send_message_event(db):
 
     await ws.disconnect()
 
-@pytest.mark.parametrize("anyio_backend", ["asyncio"])
-async def test_receive_send_title_event(db):
+@pytest.mark.asyncio
+async def test_receive_send_title_event(transactional_db):
     user, ws = await connect_to_communicator_with_user()
     chat = await user.chats.acreate(title = "Chat")
 
@@ -110,8 +110,8 @@ async def test_receive_send_title_event(db):
 
     await ws.disconnect()
 
-@pytest.mark.parametrize("anyio_backend", ["asyncio"])
-async def test_receive_send_end_event(db):
+@pytest.mark.asyncio
+async def test_receive_send_end_event(transactional_db):
     user, ws = await connect_to_communicator_with_user()
     chat = await user.chats.acreate(title = "Chat")
 
@@ -126,8 +126,8 @@ async def test_receive_send_end_event(db):
 
     await ws.disconnect()
 
-@pytest.mark.parametrize("anyio_backend", ["asyncio"])
-async def test_disconnect_removes_opened_chat(db):
+@pytest.mark.asyncio
+async def test_disconnect_removes_opened_chat(transactional_db):
     user, ws = await connect_to_communicator_with_user()
     chat = await user.chats.acreate(title = "Chat")
 
@@ -137,24 +137,24 @@ async def test_disconnect_removes_opened_chat(db):
     await ws.disconnect()
     await assert_not_in(str(chat.uuid), opened_chats)
 
-@pytest.mark.parametrize("anyio_backend", ["asyncio"])
-async def test_sending_str_closes_connection(db):
+@pytest.mark.asyncio
+async def test_sending_str_closes_connection(transactional_db):
     _, ws = await connect_to_communicator_with_user()
     await ws.send_json_to("not a dict")
     output = await ws.receive_output()
     assert output["type"] == "websocket.close"
     await ws.disconnect()
 
-@pytest.mark.parametrize("anyio_backend", ["asyncio"])
-async def test_sending_invalid_dict_closes_connection(db):
+@pytest.mark.asyncio
+async def test_sending_invalid_dict_closes_connection(transactional_db):
     _, ws = await connect_to_communicator_with_user()
     await ws.send_json_to({"chat_uuid": 123})
     output = await ws.receive_output()
     assert output["type"] == "websocket.close"
     await ws.disconnect()
 
-@pytest.mark.parametrize("anyio_backend", ["asyncio"])
-async def test_sending_invalid_chat_uuid_keeps_connection(db):
+@pytest.mark.asyncio
+async def test_sending_invalid_chat_uuid_keeps_connection(transactional_db):
     _, ws = await connect_to_communicator_with_user()
     await ws.send_json_to({"chat_uuid": str(uuid.uuid4())})
     assert await ws.receive_nothing(1, 0.05)
@@ -170,7 +170,7 @@ def get_communicator_with_cookie(user: User):
     return get_communicator([(b"cookie", get_access_cookie_for_user(user).encode())])
 
 async def connect_to_communicator_with_user():
-    user = await sync_to_async(create_user)()
+    user = await database_sync_to_async(create_user)()
     ws = get_communicator_with_cookie(user)
     connected, subprotocol = await ws.connect()
     assert connected is True
