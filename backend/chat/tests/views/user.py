efca25bd499f86da1552345545cae54ab3849324
DimensionalArray <204504974+DimensionalArray@users.noreply.github.com>
2025-12-29 14:41:54 +0000

    Improve existing AuthenticateAsGuest API view base test and add a new one to assert that the view reuses the existing guest token present in the client if it is valid

--- backend/chat/tests/views/user.py
+++ backend/chat/tests/views/user.py
@@ -883,7 +883,50 @@ class AuthenticateAsGuest(ViewsTestCase):
             self.assertEqual(response.cookies[cookie_name]["samesite"], "Lax")
             self.assertEqual(self.client.cookies[cookie_name]["samesite"], "Lax")
 
-        self.assertEqual(len(response.cookies["guest_token"].value), 36)
+        self.assertEqual(User.objects.count(), 1)
+        user: User = User.objects.first()
+
+        for cookies in [response.cookies, self.client.cookies]:
+            self.assertEqual(len(cookies["guest_token"].value), 36)
+            self.assertEqual(cookies["guest_token"].value, user.password)
+
+        self.assertEqual(len(user.email), 36 + len("@example.com"))
+        self.assertEqual(len(user.password), 36)
+        self.assertEqual(user.email, user.password + "@example.com")
+        self.assertEqual(user.password, user.email[:-len("@example.com")])
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_guest)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+        response = self.client.get("/api/me/")
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNone(response.json()["mfa"])
+
+    def test_existing_guest_token(self):
+        self.assertEqual(User.objects.count(), 0)
+        self.assertAlmostEqual(len(self.client.cookies.items()), 0)
+
+        user = User.objects.create_guest_user()
+        self.client.cookies["guest_token"] = user.password
+
+        response = self.client.post("/api/authenticate-as-guest/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b"")
+        self.assertEqual(len(response.cookies.items()), 3)
+        self.assertEqual(len(self.client.cookies.items()), 3)
+
+        for cookie_name in ["access_token", "refresh_token", "guest_token"]:
+            self.assertIn(cookie_name, response.cookies)
+            self.assertIn(cookie_name, self.client.cookies)
+            self.assertTrue(response.cookies[cookie_name]["httponly"])
+            self.assertTrue(self.client.cookies[cookie_name]["httponly"])
+            self.assertEqual(response.cookies[cookie_name]["samesite"], "Lax")
+            self.assertEqual(self.client.cookies[cookie_name]["samesite"], "Lax")
+
+        for cookies in [response.cookies, self.client.cookies]:
+            self.assertEqual(len(cookies["guest_token"].value), 36)
+            self.assertEqual(cookies["guest_token"].value, user.password)
 
         self.assertEqual(User.objects.count(), 1)
         user: User = User.objects.first()
