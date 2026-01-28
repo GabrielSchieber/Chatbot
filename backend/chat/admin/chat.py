ee69fbf8801a0569bf5bc9766abad30bce573c61
Gabriel Schieber <204504974+GabrielSchieber@users.noreply.github.com>
2026-01-28 22:38:30 +0000

    Fix ChatAdmin not allowing to select user when creating a chat via the form in the admin panel

--- backend/chat/admin/chat.py
+++ backend/chat/admin/chat.py
@@ -23,7 +23,7 @@ class ChatAdmin(admin.ModelAdmin):
 
     readonly_fields = ("user_link", "display_uuid", "pending_message_display", "created_at_display")
     fieldsets = (
-        (None, {"fields": ("user_link", "display_uuid", "title", "pending_message_display", "is_archived", "is_temporary", "created_at_display")} ),
+        (None, {"fields": ("user", "display_uuid", "title", "pending_message_display", "is_archived", "is_temporary", "created_at_display")} ),
     )
     list_display = ("title", "uuid", "user_link", "is_pending", "is_archived_display", "is_temporary_display", "created_at_display")
     search_fields = ("title", "user__email")
