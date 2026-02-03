30e2c3de76a00d205be843cd290c3b05e605a7b9
Gabriel Schieber <204504974+GabrielSchieber@users.noreply.github.com>
2026-02-03 21:54:20 +0000

    Increase the brightness of the header in the Introduction component

--- frontend/src/components/index/chat/Introduction.tsx
+++ frontend/src/components/index/chat/Introduction.tsx
@@ -10,7 +10,7 @@ export default function Introduction({ ref }: { ref: React.RefObject<HTMLHeading
         <h1
             ref={ref}
             className={`
-                fixed mb-5 text-3xl font-semibold text-center transition-opacity duration-500 text-zinc-400 light:text-zinc-600
+                fixed mb-5 text-3xl font-semibold text-center transition-opacity duration-500 text-zinc-100 light:text-zinc-900
                 ${chatUUID ? "opacity-0 pointer-events-none" : "opacity-100 pointer-events-auto"}
             `}
         >
